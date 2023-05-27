2ca27eda40d0056b4fd498f8611ba8b5966fd982
FedericoFortuna <fedekpg17@gmail.com>
2023-05-27 16:43:05 -0300

    add logs & logback.xml

--- src/main/java/com/unlam/tp_integrador/service/ProcesadorTareaServiceImpl.java
+++ src/main/java/com/unlam/tp_integrador/service/ProcesadorTareaServiceImpl.java
@@ -8,8 +8,6 @@ import com.unlam.tp_integrador.threads.ExecutorThread;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.PipedInputStream;
-import java.io.PipedOutputStream;
 
 @Service
 public class ProcesadorTareaServiceImpl implements IProcesadorTareaService {

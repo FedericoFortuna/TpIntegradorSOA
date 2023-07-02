c88a1e9aedac2996f6cebe27fbd19b3eea0ebfa1
FedericoFortuna <fedekpg17@gmail.com>
2023-07-02 20:31:04 -0300

    add front

---
+++ front/script.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {});
+
+
+  $('#combobox').on('change', function() {
+    if ($("#combobox").val() == "CUIL") {
+      document.getElementById("GENERO").style.display = "block";
+    } else {
+      document.getElementById("GENERO").style.display = "none";
+    }
+
+    if ($("#combobox").val() == "TEXT_TRANSFORM") {
+      document.getElementById("TEXT_TRANSFORM").style.display = "block";
+    } else {
+      document.getElementById("TEXT_TRANSFORM").style.display = "none";
+    }
+  });
+
+  $('#submit').click(function() {
+    event.preventDefault();
+
+
+    var inputTextValue = $('#inputText').val();
+    var comboboxValue = $('#combobox').val();
+    
+    var extraValue;
+    if ($("#combobox").val() == "CUIL") {
+      extraValue = $('#generoCUIL').val();
+    }
+    if ($("#combobox").val() == "TEXT_TRANSFORM") {
+      extraValue = $('#text_transform').val();
+    }
+    var jsonData = {
+      task_type: comboboxValue,
+      task_details: {
+        text: inputTextValue,
+        operation: extraValue
+      }
+    };
+    $.ajax({
+      url: 'http://localhost:8083/task',
+      type: 'POST',
+      data: JSON.stringify(jsonData),
+      contentType: 'application/json',
+      success: function(data) {
+        $('#inputResult').val(data);
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    });
+  });
+    
+
+
+  $('#getResult').click(function() {
+    var inputTextValue = $('#inputText').val();
+    $.ajax({
+      url: "http://localhost:8083/task/" + inputTextValue,
+      type: 'GET',
+      success: function(data) {
+        var parsedData = JSON.parse(data);
+        $('#inputResult').val(parsedData.resultado);
+      },
+      error: function(error) {
+        console.log(error);
+      }
+    });
+  });
